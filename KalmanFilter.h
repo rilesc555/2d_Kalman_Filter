115aa5fd8c136f581348d051ab43405e7b030d83
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2024-03-13 16:03:25 -0500

    initialize repo

---
+++ KalmanFilter.h
@@ -0,0 +1,16 @@
+#ifndef KALMANFILTER_H
+#define KALMANFILTER_H
+
+#include <iostream>
+#include <string>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+class KalmanFilter {
+public:
+    KalmanFilter()
+};
+
+#endif //KALMANFILTER_H
+
