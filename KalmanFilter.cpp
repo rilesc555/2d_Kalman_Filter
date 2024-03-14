927b852e05ba644405d66f964d6d184de06def16
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2024-03-14 12:25:42 -0500

    start adding matrices

---
+++ KalmanFilter.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Riley Connors on 3/14/24.
+//
+#include "KalmanFilter.h"
+
+KalmanFilter::KalmanFilter() {
+
+}
+
--- KalmanFilter.h
+++ KalmanFilter.h
@@ -8,8 +8,29 @@
 using namespace Eigen;
 
 class KalmanFilter {
+
 public:
-    KalmanFilter()
+    KalmanFilter();
+
+private:
+    //tracks time step
+    unsigned int k;
+
+    // m - input dimension, n - state dimension, r - output dimension
+    unsigned int m, n, r;
+
+    // ABC are system matrices, QR are noise covariance matrices, P0 is initial state covariance
+    MatrixXd A, B, C, Q, R, P0;
+    // x0 is initial state
+    MatrixXd x0;
+
+    MatrixXd estimatesAposteriori, estimatesApriori;
+
+    MatrixXd covarianceAposteriori, covarianceApriori;
+
+    MatrixXd kalmanGain;
+
+    MatrixXd errors;
 };
 
 #endif //KALMANFILTER_H
