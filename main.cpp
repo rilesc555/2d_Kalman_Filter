b112227708b5e4578a82f097fc165875c1b3a481
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2024-03-21 12:00:12 -0500

    get data from logs

--- main.cpp
+++ main.cpp
@@ -7,9 +7,47 @@
 #include <map>
 #include <string>
 #include <vector>
-#include "single_include/csv.hpp"
+#include "rapidcsv.h"
 
 int main() {
+    rapidcsv::Document doc("tracks.csv");
+    std::vector<double> id, vx, vy, vz, az, el;
+
+    id = doc.GetColumn<double>("track_id");
+    vx = doc.GetColumn<double>("vel_x");
+    vy = doc.GetColumn<double>("vel_y");
+    vz = doc.GetColumn<double>("vel_z");
+    az = doc.GetColumn<double>("az");
+    el = doc.GetColumn<double>("el");
+
+    Eigen::MatrixXd F(id.size(), 6);
+    int inc = 0;
+    for (int i = 0; i < id.size(); i++) {
+        if (id.at(i) == 102) {
+            F(inc, 0) = id.at(i);
+            F(inc, 1) = vx.at(i);
+            F(inc, 2) = vy.at(i);
+            F(inc, 3) = vz.at(i);
+            F(inc, 4) = az.at(i);
+            F(inc, 5) = el.at(i);
+            inc++;
+        }
+        else {
+            continue;
+        }
+    }
+
+    Eigen::MatrixXd P = F.block(0, 0, inc, 6);
+
+    Eigen::MatrixXd A(7, 7);
+    A << 1, 0, 0, 0, 0, 0, 0,
+         0, 1, 0, 0, 0, 0, 0,
+         0, 0, 1, 0, 0, 0, 0,
+         0, 0, 0, 1, 0, 0, 0,
+         0, 0, 0, 0, 1, 0, 0,
+         0, 0, 0, 0, 0, 1, 0,
+         0, 0, 0, 0, 0, 0, 1;
+
 
 }
 
